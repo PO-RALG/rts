be33f78ccd6a157992b55fcc4edb38c50b8f555c
Mfaume Mnokote <mmnokote@gmail.com>
2024-03-18 19:07:20 +0300

    signal table

--- djangoProjectTest/settings.py
+++ djangoProjectTest/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'trs',
+        'NAME': 'rts',
         'USER': 'postgres',
         'PASSWORD': 'Evlina@1990',
         'HOST': 'localhost',
